f113f28c1316afaf788b9ea4d533fbe9f97f0572
Belen <gonzaleznovoa.b@protonmail.com>
2023-03-17 11:59:35 +0100

    changing the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,40 +1,46 @@
-import React from 'react' //rfc créer nouveau dossier avec les import et export
+import React from 'react'; //rfc créer nouveau dossier avec les import et export
 
 export default function Footer() {
   return (
     <footer>
+     
       <div>
         <a 
-          className="" //pour rappeler les classes de style.css
-          href="/"
+          className="footerlinks" //pour rappeler les classes de style.css
+          href="https://drdrone.fr/#services"
+          rel="noreferrer"
           target="_blank" //permet quand clique sur le lien, d'ouvrir une nouvelle page !
         > 
           Nos prestations
         </a>
         <a 
-          className=""
-          href="/"
+          className="footerlinks"
+          href="https://drdrone.fr/qui-sommes-nous/"
+          rel="noreferrer"
           target="_blank"
         > 
           Qui sommes nous ?
         </a>
         <a
-         className=""
-         href="/"
+         className="footerlinks"
+         href="https://drdrone.fr/demande-de-devis/"
+         rel="noreferrer"
          target="_blank"
         >
            Demande de devis
         </a>
         <a 
-          className=""
-          href="/"
+          className="footerlinks"
+          href="https://drdrone.fr/contact/"
+          rel="noreferrer"
           target="_blank"
         >
            Contact
         </a>
         <a 
-          className=""
-          href="/"
+          className="footerlinks"
+          href="https://drdrone.fr/mentions-legales/"
+          rel="noreferrer"
           target="_blank"
         >
            Mentions légales
